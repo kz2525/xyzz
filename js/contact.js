265914b003097bc2b69d3415a70942a689b06c35
kz2525 <codexdua@tuta.io>
2023-12-27 23:32:42 +0500

    xyz

---
+++ js/contact.js
@@ -0,0 +1,183 @@
+// Contact Form
+function validateForm() {
+  var name = document.forms["myForm"]["name"].value;
+  var email = document.forms["myForm"]["email"].value;
+  var subject = document.forms["myForm"]["subject"].value;
+  var comments = document.forms["myForm"]["comments"].value;
+  document.getElementById("error-msg").style.opacity = 0;
+  document.getElementById('error-msg').innerHTML = "";
+  if (name == "" || name == null) {
+    document.getElementById('error-msg').innerHTML = "<div class='alert alert-warning error_message'>*Please enter a Name*</div>";
+    fadeIn();
+    return false;
+  }
+  if (email == "" || email == null) {
+    document.getElementById('error-msg').innerHTML = "<div class='alert alert-warning error_message'>*Please enter a Email*</div>";
+    fadeIn();
+    return false;
+  }
+  if (subject == "" || subject == null) {
+    document.getElementById('error-msg').innerHTML = "<div class='alert alert-warning error_message'>*Please enter a Subject*</div>";
+    fadeIn();
+    return false;
+  }
+  if (comments == "" || comments == null) {
+    document.getElementById('error-msg').innerHTML = "<div class='alert alert-warning error_message'>*Please enter a Comments*</div>";
+    fadeIn();
+    return false;
+  }
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("simple-msg").innerHTML = this.responseText;
+      document.forms["myForm"]["name"].value = "";
+      document.forms["myForm"]["email"].value = "";
+      document.forms["myForm"]["subject"].value = "";
+      document.forms["myForm"]["comments"].value = "";
+    }
+  };
+  xhttp.open("POST", "php/contact.php", true);
+  xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  xhttp.send("name=" + name + "&email=" + email + "&subject=" + subject + "&comments=" + comments);
+  return false;
+}
+function fadeIn() {
+  var fade = document.getElementById("error-msg");
+  var opacity = 0;
+  var intervalID = setInterval(function () {
+    if (opacity < 1) {
+      opacity = opacity + 0.5
+      fade.style.opacity = opacity;
+    } else {
+      clearInterval(intervalID);
+    }
+  }, 200);
+}
+
+
+$(document).ready(function() {
+  $('#contact_form').bootstrapValidator({
+      // To use feedback icons, ensure that you use Bootstrap v3.1.0 or later
+      feedbackIcons: {
+          valid: 'glyphicon glyphicon-ok',
+          invalid: 'glyphicon glyphicon-remove',
+          validating: 'glyphicon glyphicon-refresh'
+      },
+      fields: {
+          first_name: {
+              validators: {
+                      stringLength: {
+                      min: 2,
+                  },
+                      notEmpty: {
+                      message: 'Please supply your first name'
+                  }
+              }
+          },
+           last_name: {
+              validators: {
+                   stringLength: {
+                      min: 2,
+                  },
+                  notEmpty: {
+                      message: 'Please supply your last name'
+                  }
+              }
+          },
+          email: {
+              validators: {
+                  notEmpty: {
+                      message: 'Please supply your email address'
+                  },
+                  emailAddress: {
+                      message: 'Please supply a valid email address'
+                  }
+              }
+          },
+          phone: {
+              validators: {
+                  notEmpty: {
+                      message: 'Please supply your phone number'
+                  },
+                  phone: {
+                      country: 'US',
+                      message: 'Please supply a vaild phone number with area code'
+                  }
+              }
+          },
+          address: {
+              validators: {
+                   stringLength: {
+                      min: 8,
+                  },
+                  notEmpty: {
+                      message: 'Please supply your street address'
+                  }
+              }
+          },
+          city: {
+              validators: {
+                   stringLength: {
+                      min: 4,
+                  },
+                  notEmpty: {
+                      message: 'Please supply your city'
+                  }
+              }
+          },
+          state: {
+              validators: {
+                  notEmpty: {
+                      message: 'Please select your state'
+                  }
+              }
+          },
+          zip: {
+              validators: {
+                  notEmpty: {
+                      message: 'Please supply your zip code'
+                  },
+                  zipCode: {
+                      country: 'US',
+                      message: 'Please supply a vaild zip code'
+                  }
+              }
+          },
+          comment: {
+              validators: {
+                    stringLength: {
+                      min: 10,
+                      max: 200,
+                      message:'Please enter at least 10 characters and no more than 200'
+                  },
+                  notEmpty: {
+                      message: 'Please supply a description of your project'
+                  }
+                  }
+              }
+          }
+      })
+      .on('success.form.bv', function(e) {
+          $('#success_message').slideDown({ opacity: "show" }, "slow") // Do something ...
+              $('#contact_form').data('bootstrapValidator').resetForm();
+
+          // Prevent form submission
+          e.preventDefault();
+
+          // Get the form instance
+          var $form = $(e.target);
+
+          // Get the BootstrapValidator instance
+          var bv = $form.data('bootstrapValidator');
+
+          // Use Ajax to submit form data
+          $.post($form.attr('action'), $form.serialize(), function(result) {
+              console.log(result);
+          }, 'json');
+      });
+});
+
+
+
+
+
